f5b85e29f75d06a576d93fbeec9fd9ae8ddaf5cb
Richardryanp <Richardryan2050@gmail.com>
2026-01-04 18:29:10 +0700

    Add login feature

---
+++ TPM_Login/Login_logout/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+import authRouter from "./routes/auth.route.js";
+
+app.use("/api/auth", authRouter);
+
+export default app;
+
